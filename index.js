e818dff89ef7845bda46cc504cf849bb29bce91d
Facundo Sura <facundomesura@gmail.com>
2025-02-12 19:48:52 -0300

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 const axios = require("axios");
 const server = require("./src/server");
-const { conn } = require('./src/db.js');
+const { conn } = require("./src/db.js");
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
-server.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on port ${port}`);
-})
-}).catch(error => console.error(error))
+conn
+  .sync({ force: true })
+  .then(() => {
+    server.listen(port, '0.0.0.0', () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => console.error(error));
